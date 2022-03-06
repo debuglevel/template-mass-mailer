7e105dfda86eb7d5bc227bc2a7b02c08097fa4b6
Marc Kohaupt <debuglevel@gmail.com>
2022-03-06 11:49:04 +0100

    Added basic functionality

--- main.py
+++ main.py
@@ -1,16 +1,95 @@
-# This is a sample Python script.
+import email
+import logging.config
+from pprint import pprint
+from typing import Optional, List, Dict
+import csv
+import pandas
+from jinja2 import Template
+import smtplib
+import ssl
 
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+def get_mail_entries(csv_filename) -> List[Dict]:
+    logger.debug(f"Getting entries from {csv_filename}...")
+
+    mail_entries = []
+
+    logger.debug(f"Reading CSV {csv_filename}...")
+    df = pandas.read_csv(csv_filename)
+    for index, row in df.iterrows():
+        logger.debug(f"Converting row {index} to dictionary...")
+        row_dict = row.to_dict()
+        mail_entries.append(row_dict)
+
+    logger.debug(f"Got {len(mail_entries)} entries from {csv_filename}")
+    return mail_entries
+
+
+def get_mail_body(template, mail_entry) -> str:
+    logger.debug(f"Getting mail body...")
+
+    with open('template.jinja2') as file:
+        template = Template(file.read())
+
+    mail_body = template.render(x=mail_entry)
+
+    return mail_body
+
+
+def send_mail(mail_address, mail_body, smtp_configuration):
+    logger.debug(f"Sending mail to {mail_address}...")
+
+    msg = email.message.Message()
+    msg['From'] = smtp_configuration["from"]
+    msg['To'] = mail_address
+    msg['Subject'] = smtp_configuration["subject"]
+    msg.add_header('Content-Type', 'text')
+    msg.set_payload(mail_body)
+
+    context = ssl.create_default_context()
+    # Try to log in to server and send email
+    try:
+        server = smtplib.SMTP(smtp_configuration["server"], smtp_configuration["port"])
+        server.ehlo()  # check connection
+        # server.starttls(context=context)  # Secure the connection
+        server.ehlo()  # check connection
+        # server.login(smtp_configuration["username"], smtp_configuration["password"])
+
+        # Send email here
+        server.sendmail(smtp_configuration["from"], mail_address, msg.as_string())
+    except Exception as e:
+        # Print any error messages to stdout
+        print(e)
+    finally:
+        server.quit()
+
+
+def main():
+    logger.debug(f"Running main...")
+
+    csv_filename = "list.csv"
+    mail_entries = get_mail_entries(csv_filename)
+
+    template = "template.jinja2"
+
+    smtp_configuration = {
+        'server': "mail.smtpbucket.com",
+        "port": 8025,
+        "username": "",
+        "password": "",
+        "from": "the@establishment.invalid",
+        "subject": "Your beautiful subject",
+    }
+
+    for mail_entry in mail_entries:
+        mail_address = mail_entry['email']
+        mail_body = get_mail_body(template, mail_entry)
+        send_mail(mail_address, mail_body, smtp_configuration)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
